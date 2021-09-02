8de0af75a93488c649b59a5a46da81f6489fc266
koopReynders <mail@kopo.nl>
2021-09-03 00:02:22 +0200

    tooltip watisdat toegevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
 console.log("kopf")
 
 var container = document.querySelector("body");
-var hit = document.querySelector("div#hit");
-var blup = document.querySelector("div#blup");
-var kopimg = document.querySelector("img");
+var hit = document.querySelector("div#hit"); //hit area voor de blup
+var blup = document.querySelector("div#blup"); //het ronde blup ding
+var kopimg = document.querySelector("section#kop img"); //img met de kop
 var kop1 ="assets/KOP1.JPG"; //mond hap hap
 var kop2 ="assets/KOP2.JPG"; //mond open
 var kop3 ="assets/KOP3.JPG"; //mond
@@ -12,29 +12,34 @@ var hapsec = 0.160; //haphaphap animatie in seconden
 var reset; //timeout na klik
 var resetsec = 8; //timeout reset in seconden, hoe lang iss de muis niet zichtbaar
 
+//onclick meer informatie
 var meer = document.querySelector("button+img");
 document.querySelector("button").onclick = function() {
     console.log("click click click");
     this.classList.add("none");
     meer.classList.add("none");
     container.classList.add("move");
+    wegmetwatisdat();
 }
 
+function wegmetwatisdat(){
+    var watisdat = document.querySelector("img[src*=watisdat]"); //tooltip die na een aantal seconden verschijnt
+    watisdat.classList.add("none"); //tooltip op display none zetten na een een mousover of klik op info
+}
 
-
-
-function happer(){
+function haphaphap() { //muis wordt opgegeten met een hap
+    kopimg.src = kop1 //mond hap hap
+    hit.removeEventListener("mouseover", happer); //onmouseover weghalen
+    blup.classList.replace("rollover","hide"); //blup ding onzichtbaar maken
+    container.classList.replace("cursorpointer","cursornone"); //cursor niet zichtbaar maken
+}
+function happer(){ //mouseover functie
     kopimg.src=kop2; //mond open
     blup.classList.add("rollover"); //blup ding wit maken met rollover
     container.classList.add("cursorpointer"); //cursor wordt handje
     hap = setTimeout(haphaphap, hapsec*1000); //timeout voor de hap
     reset = setTimeout(setup, resetsec*1000); //timeout in miliseconden voor reset
-}
-function haphaphap() {
-    kopimg.src = kop1 //mond hap hap
-    hit.removeEventListener("mouseover", happer); //onmouseover weghalen
-    blup.classList.replace("rollover","hide"); //blup ding onzichtbaar maken
-    container.classList.replace("cursorpointer","cursornone"); //cursor niet zichtbaar maken
+    wegmetwatisdat();
 }
 function setup(){
     kopimg.src = kop3; //mond
